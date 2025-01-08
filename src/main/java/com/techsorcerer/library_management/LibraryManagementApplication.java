94646ac2d442d920cb2873004d8490ec20fa8a04
Mayank <mayankpatil2605@gmail.com>
2025-01-07 16:01:42 -0800

    Added ModelMapperConfig to inject modelMapper directly

--- src/main/java/com/techsorcerer/library_management/LibraryManagementApplication.java
+++ src/main/java/com/techsorcerer/library_management/LibraryManagementApplication.java
@@ -2,9 +2,16 @@ package com.techsorcerer.library_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibraryManagementApplication {
+public class LibraryManagementApplication extends SpringBootServletInitializer  {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(LibraryManagementApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementApplication.class, args);
